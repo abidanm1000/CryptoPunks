e1dbff24714d5c39bd0e7a3eb1dfa399e15a4bc5
Abidan Martinez <abidanmtz@gmail.com>
2022-02-27 02:51:48 -0800

    updating files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
-import CollectionCard from './components/CollectionCard';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Punklist from './components/Punklist';
@@ -27,13 +25,13 @@ function App() {
     <div className='app'>
       <Header />
       {punkListData.length > 0 && (
-        <>
+        <div>
           <Main punkListData={punkListData} selectedPunk={selectedPunk}/>
           <Punklist 
             punkListData={punkListData} 
             setSelectedPunk={setSelectedPunk} 
           />
-        </>
+        </div>
       )}
       
     </div>)
